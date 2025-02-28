8a05972668b299ef07237d5f7a53281e8b1ce55f
Rwhy14 <ryanchou.texas@gmail.com>
2025-02-28 16:57:25 -0500

    1

--- doc/02-component-proof-of-concept/02-component-proof-of-concept.md
+++ doc/02-component-proof-of-concept/02-component-proof-of-concept.md
@@ -1,12 +1,9 @@
 # Portfolio Part 2: Component Proof-of-Concept
 
-- **Name**: <!-- TODO: fill with first and last name (e.g., Brutus Buckeye) then delete this comment -->
-- **Dot Number**: <!-- TODO: fill with OSU dot number (e.g., buckeye.17) then delete this comment -->
-- **Due Date**: <!-- TODO: fill out with due date and time (e.g., 10/17 @ 3:10 PM EST) then delete this comment -->
-
+- **Name**: Ryan Chou
+- **Dot Number**: chou.391
+- **Due Date**: 2/20 @ 8:00 PM EST
 ## Assignment Overview
-
-<!-- TODO: read the assignment overview then delete this comment -->
 
 Previously, you brainstormed three ideas, and hopefully you got some feedback as
 well. However, it's impossible to know how reasonable your design actually is
@@ -30,8 +27,6 @@ the more work you can put in now, the better.
 
 ## Assignment Checklist
 
-<!-- TODO: browse the checklist then delete this comment -->
-
 To be sure you have completed everything on this assignment, we have littered
 this document with TODO comments. You can browse all of them in VSCode by
 opening the TODOs window from the sidebar. The icon looks like a tree and will
@@ -53,8 +48,6 @@ to the tree diagram (you may remove this one as well):
 
 ## Assignment Learning Objectives
 
-<!-- TODO: read the assignment learning objectives then delete this comment -->
-
 Without learning objectives, there really is no clear reason why a particular
 assessment or activity exists. Therefore, to be completely transparent, here is
 what we're hoping you will learn through this particular aspect of the portfolio
@@ -67,8 +60,6 @@ project. Specifically, students should be able to:
 3. Assemble a minimal working implementation of one of their designs
 
 ## Assignment Rubric: 10 Points
-
-<!-- TODO: read the assignment rubric then delete this comment -->
 
 Again, to be completely transparent, most of the portfolio project, except the
 final submission, is designed as a formative assessment. Formative assessments
@@ -114,8 +105,7 @@ Below is further rationale/explanation for the rubric items above:
 > to create a new design. In you do end up picking one at random, you should
 > disclose that here as well.
 
-<!-- TODO: briefly argue your choice of design or design something
-new; then delete this comment -->
+In the beginning I was more toward choosing Virus Inflection Tracker as my design since I would want to do a project related to bio. However, I figured that I didn't have a general idea of how to collect the data from worldwide which means that I didn't target a certain population and region for my project. One of the considerations will be time usage. Thus, I chose player stats tracker as my choice since I already focused on certain players and it will be more limited (if that makes sense). For nutrition tracker, I just don't find that interesting at all.
 
 > Once you've argued your choice of design, make a branch in your new repo called
 > something like `proof-of-concept`. There are many ways to do this, but my
@@ -126,7 +116,6 @@ new; then delete this comment -->
 > we'll want a branch that you can later make a pull request from with all
 > your changes.
 
-<!-- TODO: make a new branch from main then delete this comment -->
 
 ## Assignment Tasks
 
@@ -153,7 +142,6 @@ completed the assignment.
 
 ### Changelog
 
-<!-- TODO: update CHANGELOG then delete this comment -->
 
 At the end of every assignment, you should update the
 [CHANGELOG.md](../../CHANGELOG.md) file found in the root of the project folder.
@@ -168,15 +156,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Calendar Versioning](https://calver.org/) of
 the following form: YYYY.0M.0D.
 
-## YYYY.MM.DD
+## 2025.02.19
 
 ### Added
 
-- Designed a proof of concept for <!-- insert name of component here --> component
+- Designed a proof of concept for player stats tracker component
 
 ### Updated
 
-- Changed design to include ...
+- Changed design to include basic concepts and an example of main method.
+- Created a java file
 
 ```
 
--- src/conponent/src/App.java
+++ src/conponent/src/App.java
@@ -1,5 +1,120 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerStatsTracker {
+
+    // Internal representation: using a map to store stat names and their cumulative values.
+    // This structure is mutable because player stats change after each game.
+    private Map<String, Double> stats;
+    private int totalGames;
+    private double wins;
+
+    // Constructor
+    public PlayerStatsTracker() {
+        this.stats = new HashMap<>();
+        this.totalGames = 0;
+        this.wins = 0.0;
     }
+
+    // Kernel Methods
+
+    // Adds or updates a stat by adding the given value to any existing value.
+    public void addStat(String stat, double value) {
+        double current = this.stats.getOrDefault(stat, 0.0);
+        this.stats.put(stat, current + value);
+    }
+
+    // Removes a stat from the tracker.
+    public void removeStat(String stat) {
+        this.stats.remove(stat);
+    }
+
+    // Returns the total for a given stat; if not found, returns 0.
+    public double getStatTotal(String stat) {
+        return this.stats.getOrDefault(stat, 0.0);
+    }
+
+    // Increases the count of games played and, if the game was a win, increments win count.
+    public void recordGame(boolean win) {
+        this.totalGames++;
+        if (win) {
+            this.wins++;
+        }
+    }
+
+    // Returns the win rate calculated as wins divided by total games played.
+    public double getWinRate() {
+        if (this.totalGames == 0) {
+            return 0;
+        }
+        return this.wins / this.totalGames;
+    }
+
+    // Returns the total number of games played.
+    public int totalGamePlayed() {
+        return this.totalGames;
+    }
+
+    // Secondary Methods
+
+    // Calculates the average for a given stat over a specified number of games.
+    // Example: average points per game.
+    public double getAverageStat(String stat, int gamesPlayed) {
+        if (gamesPlayed <= 0) {
+            return 0;
+        }
+        return this.getStatTotal(stat) / gamesPlayed;
+    }
+
+    // Displays all stats for the player.
+    public void displayStats() {
+        System.out.println("Player Stats:");
+        for (Map.Entry<String, Double> entry : this.stats.entrySet()) {
+            System.out
+                    .println(" - " + entry.getKey() + ": " + entry.getValue());
+        }
+        System.out.println("Games Played: " + this.totalGamePlayed());
+        System.out.println("Win Rate: " + this.getWinRate());
+    }
+
+    // Example secondary method that uses getWinRate() kernel method to provide a calculated win rate.
+    public double calculatedWinRate() {
+        // For demonstration, it simply calls getWinRate()
+        return this.getWinRate();
+    }
+
+    // Calculates average plus-minus value per game. Assumes a stat named "plusMinus" exists.
+    public double avPlusMinus() {
+        return this.getAverageStat("plusMinus", this.totalGamePlayed());
+    }
+
+    // Main method demonstrating the component in action.
+    public static void main(String[] args) {
+        // Create an instance of PlayerStatsTracker
+        PlayerStatsTracker tracker = new PlayerStatsTracker();
+
+        // Record some games and stats
+        tracker.recordGame(true);   // Game 1: win
+        tracker.addStat("points", 20);
+        tracker.addStat("rebounds", 10);
+        tracker.addStat("plusMinus", 5);
+
+        tracker.recordGame(false);  // Game 2: loss
+        tracker.addStat("points", 15);
+        tracker.addStat("rebounds", 8);
+        tracker.addStat("plusMinus", -3);
+
+        tracker.recordGame(true);   // Game 3: win
+        tracker.addStat("points", 25);
+        tracker.addStat("rebounds", 12);
+        tracker.addStat("plusMinus", 7);
+
+        // Display the current stats
+        tracker.displayStats();
+
+        // Example usage of secondary methods
+        System.out.println("Calculated Win Rate: " + tracker.calculatedWinRate());
+        System.out.println("Average Plus/Minus: " + tracker.avPlusMinus());
+
+
 }
